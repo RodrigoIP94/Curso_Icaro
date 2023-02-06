5d7874a751ee2eb65907a42279b60fff384551e9
Rodrigo Iriarte <rodrigo.ip@hotmail.com>
2023-02-05 21:05:57 -0300

    agrego archivo practica

---
+++ 00_Tareas/01_Tarea_Git/Practica.js
@@ -0,0 +1,9 @@
+let num1 = 12
+let num2 = 23
+
+function Sumar(num1, num2){
+    let resultado = num1 + num2;
+    return (resultado);
+}
+
+console.log("el resultado de num1 + num2 es: ", resultado);
